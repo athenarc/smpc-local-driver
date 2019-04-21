db27a2df97f4b8a47285c7d8c2558b0ea1463791
Christos Nasikas <xristosnasikas@gmail.com>
2019-04-22 00:09:37 +0300

    Test preprocessor

---
+++ scripts/test.py
@@ -0,0 +1,58 @@
+import unittest
+import sys
+
+from preprocessor import preprocess
+
+categorical = [
+    'Ethnicity',
+    'Gender',
+]
+
+numerical = [
+    'Patient Age',
+    'Heart rate',
+    'Height (cm)',
+    'Weight (kg)',
+    'LVEDV (ml)',
+    'LVESV (ml)',
+    'LVSV (ml)',
+    'LVEF (%)',
+    'LV Mass (g)',
+    'RVEDV (ml)',
+    'RVESV (ml)',
+    'RVSV (ml)',
+    'RVEF (%)',
+    'RV Mass (g)',
+    'BMI (kg/msq)',
+    'BMI (kg/m²)',
+    'BSA',
+    'BSA (msq)',
+    'CO (L/min)',
+    'Central PP (mmHg)',
+    'DBP (mmHg)',
+    'LVEF (ratio)',
+    'MAP',
+    'PAP (mmHg)',
+    'PP (mmHg)',
+    'RVEF (ratio)',
+    'SBP (mmHg)',
+    'SVR (mmHg/L/min)'
+]
+
+
+class TestSum(unittest.TestCase):
+    def test_categorical(self):
+        for attr in categorical:
+            print('---------------------Testing attribute: ' + attr + '---------------------')
+            preprocess('1d_categorical_histogram', 'test', [attr], sys.argv[1], '../smpc-global/mapping.json', 5)
+            print('---------------------Done!---------------------')
+
+    def test_numerical(self):
+        for attr in numerical:
+            print('---------------------Testing attribute: ' + attr + '---------------------')
+            preprocess('1d_numerical_histogram', 'test', [attr], sys.argv[1], '../smpc-global/mapping.json', 5)
+            print('---------------------Done!---------------------')
+
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
