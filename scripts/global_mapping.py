47d41a943c76ae03aa8b78952a33b68adfe4b9c5
Christos Nasikas <xristosnasikas@gmail.com>
2019-06-13 15:46:22 +0300

    Refactor global mapping

--- scripts/global_mapping.py
+++ scripts/global_mapping.py
@@ -5,38 +5,17 @@ import argparse
 from utils import read_json, write_json
 
 
-def parse_unique_id(mesh_terms):
-    counter = 0
-    global_map = {}
-
-    for term in mesh_terms:
-        global_map[term['code']] = counter
-        counter += 1
-
-    return global_map
-
-
-def parse_tree_numbers(mesh_terms):
-    counter = 0
-    global_map = {}
-
-    for term in mesh_terms:
-        for id in term['ids']:
-            if id not in global_map:
-                global_map[id] = counter
-                counter += 1
-
-    return global_map
-
-
 def map_mesh_terms(args):
     global_map = {}
 
     mesh_terms = read_json(args.mesh)
-    if args.unique:
-        global_map = parse_unique_id(mesh_terms)
-    else:
-        global_map = parse_tree_numbers(mesh_terms)
+
+    for term in mesh_terms:
+        global_map[term['id']] = {}
+        counter = 0
+        for child in term['children']:
+            global_map[term['id']][child['id']] = counter
+            counter += 1
 
     write_json(args.output, global_map)
 
@@ -44,13 +23,8 @@ def map_mesh_terms(args):
 def main():
     parser = argparse.ArgumentParser(
         description='SMPC global mapping generator')
-    parser.add_argument('mesh', help='File containing mesh terms in XML format as defined in ftp://nlmpubs.nlm.nih.gov/online/mesh')
+    parser.add_argument('mesh', help='File containing mesh terms with their childer. See catalogue.py --help.')
     parser.add_argument('output', help='Output file (JSON)')
-    parser.add_argument(
-        '-u',
-        '--unique',
-        action='store_true',
-        help='Create a mapping from Mesh Unique ID')
     parser.add_argument('--version', action='version', version='%(prog)s 0.2')
     args = parser.parse_args()
     map_mesh_terms(args)
