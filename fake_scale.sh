9a70f35ef0c039e3586214538cdb89c25a21e841
Christos Nasikas <xristosnasikas@gmail.com>
2019-06-18 19:50:38 +0300

    Create fake scale script

---
+++ fake_scale.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo '[*] Connecting to players...'
+sleep 0.5
+echo '[*] Receiving triples...'
+sleep 0.5
+echo '[*] Verifing triples...'
+sleep 0.5
+echo '[*] Send data'
+sleep 0.5
+echo '[*] Done!'
