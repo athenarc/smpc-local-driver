f3e7f30dcac72e4482868ef6bebd228809581e7e
Christos Nasikas <xristosnasikas@gmail.com>
2019-06-19 19:27:45 +0300

    Change attributes

--- scripts/test_preprocessor.py
+++ scripts/test_preprocessor.py
@@ -3,8 +3,8 @@ from preprocessor import preprocess
 computations = {
     '1d_categorical_histogram': {'id': 'test_id_1', 'attributes': ['C14.280']},
     '2d_categorical_histogram': {'id': 'test_id_2', 'attributes': ['C14.280', 'C13.703']},
-    '1d_numerical_histogram': {'id': 'test_id_3', 'attributes': ['M01.060']},
-    '2d_numerical_histogram': {'id': 'test_id_4', 'attributes': ['M01.060', 'E01.370.600.115.100.160.120']}
+    '1d_numerical_histogram': {'id': 'test_id_3', 'attributes': ['C23.888.144']},
+    '2d_numerical_histogram': {'id': 'test_id_4', 'attributes': ['C23.888.144', 'Z01.252.100.450']}
 }
 
 for c in computations:
