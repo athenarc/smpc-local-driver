2c63f94d44755b1501eecb69e9ab0ec3f427bead
GPikra <george.pikramenos@hotmail.com>
2019-07-01 17:10:18 +0300

    fix 2d

--- scripts/test_preprocessor.py
+++ scripts/test_preprocessor.py
@@ -3,7 +3,7 @@ from preprocessor import preprocess
 import utils
 computations = {
     '1d_categorical_histogram': {'id': 'test_id_1', 'attributes': ['C14.280']}
-    # '2d_categorical_histogram': {'id': 'test_id_2', 'attributes': ['C14.280', 'C13.703']},
+    # '2d_categorical_histogram': {'id': 'test_id_2', 'attributes': ['C14.280', 'C13.703']}
     # '1d_numerical_histogram': {'id': 'test_id_3', 'attributes': ['C23.888.144']},
     # '2d_numerical_histogram': {'id': 'test_id_4', 'attributes': ['C23.888.144', 'Z01.252.100.450']}
 }
