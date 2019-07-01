5b60a82a08e7ec5803003fafb74a3ad0eb4700ba
GPikra <george.pikramenos@hotmail.com>
2019-07-01 16:14:56 +0300

    call api for 1dcat

--- scripts/test_preprocessor.py
+++ scripts/test_preprocessor.py
@@ -2,10 +2,10 @@ import sys
 from preprocessor import preprocess
 
 computations = {
-    '1d_categorical_histogram': {'id': 'test_id_1', 'attributes': ['C14.280']},
-    '2d_categorical_histogram': {'id': 'test_id_2', 'attributes': ['C14.280', 'C13.703']},
-    '1d_numerical_histogram': {'id': 'test_id_3', 'attributes': ['C23.888.144']},
-    '2d_numerical_histogram': {'id': 'test_id_4', 'attributes': ['C23.888.144', 'Z01.252.100.450']}
+    '1d_categorical_histogram': {'id': 'test_id_1', 'attributes': ['C14.280']}
+    # '2d_categorical_histogram': {'id': 'test_id_2', 'attributes': ['C14.280', 'C13.703']},
+    # '1d_numerical_histogram': {'id': 'test_id_3', 'attributes': ['C23.888.144']},
+    # '2d_numerical_histogram': {'id': 'test_id_4', 'attributes': ['C23.888.144', 'Z01.252.100.450']}
 }
 
 for c in computations:
