5b670147169d987f7e006d1db7203e40d0c8564c
Christos Nasikas <xristosnasikas@gmail.com>
2019-07-02 12:38:34 +0300

    Create strategy pattern

---
+++ scripts/algorithms.py
@@ -0,0 +1,65 @@
+import abc
+class Processor:
+    def __init__(self, strategy):
+        self._strategy = strategy
+
+    def process(self):
+        return self._strategy.process()
+
+
+class Attribute:
+    def __init__(self, id, name, code):
+        self.id = id
+        self.name = name
+        self.code = code
+
+
+class Strategy(metaclass=abc.ABCMeta):
+    def __init__(self, id, attributes, dataset, precision=5):
+        self._id = id
+        self._rattributes = attributes
+        self._dataset = dataset
+        self.__precision = precision
+        self._attributes = None
+
+    @abc.abstractmethod
+    def validate(self):
+        pass
+
+    def process_attributes(self):
+        self._attributes = [Attribute(MESH_TERMS[a]['id'], MESH_TERMS[a]['name'], MESH_TERMS[a]['code']) for a in self._rattributes]
+
+    def get_attributes_by_key(self, key):
+        if self._attributes is None:
+            return []
+
+        return [getattr(a, key) for a in self._attributes]
+
+    @abc.abstractmethod
+    def process(self):
+        pass
+
+    @abc.abstractmethod
+    def out(self):
+        pass
+
+
+class OneDimensionCategoricalHistogram(Strategy):
+
+    def out(self):
+        pass
+
+
+class TwoDimensionCategoricalHistogram(Strategy):
+    def process(self):
+        pass
+
+
+class OneDimensionNumericalHistogram(Strategy):
+    def process(self):
+        pass
+
+
+class TwoDimensionNumericalHistogram(Strategy):
+    def process(self):
+        pass
