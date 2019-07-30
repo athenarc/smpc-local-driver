d46b23e3de0e2b203120edf0fad41052fe9b6104
Christos Nasikas <xristosnasikas@gmail.com>
2019-07-30 18:50:39 +0300

    Pass json.dump instead of string

--- scripts/test_preprocessor.py
+++ scripts/test_preprocessor.py
@@ -3,6 +3,7 @@
 import sys
 from preprocessor import preprocess
 from collections import namedtuple
+import json
 
 Arguments = namedtuple('args', ('algorithm', 'computation', 'attributes', 'dataset', 'precision', 'request'))
 
@@ -11,17 +12,16 @@ computations = {
         'id': 'test_id_1',
         'attributes': ['C14.280'],
         'request':
-        # A string of request description. The string will be json decoded.
-        '''
+        json.dumps(
             {
-                "attributes":[{"name":"C14.280"}],
-                "algorithm":"1d_categorical_histogram",
-                "dataProviders":[0],
-                "link":"http://example.com/download",
-                "id":"e6e0f024-9879-4400-823e-b9db0db3af68",
-                "timestamps":{"accepted":1562145951528,"process":1562145951535},
-                "status":"1",
-                "totalClients":"1",
+                "attributes": [{"name": "C14.280"}],
+                "algorithm": "1d_categorical_histogram",
+                "dataProviders": [0],
+                "link": "http://example.com/download",
+                "id": "e6e0f024-9879-4400-823e-b9db0db3af68",
+                "timestamps": {"accepted": 1562145951528, "process": 1562145951535},
+                "status": "1",
+                "totalClients": "1",
                 "raw_request": [{
                     "consent": "marketing",
                     "datatype": "crs_identified,Magnetic Resonance Imaging (MRI) of Heart,Cardiac CT",
@@ -36,7 +36,7 @@ computations = {
                     "keywords": ["Heart Diseases"]
                 }]
             }
-        '''
+        )
     },
     '2d_categorical_histogram': {'id': 'test_id_2', 'attributes': ['C14.280', 'C13.703'], 'request': None},
     '1d_numerical_histogram': {'id': 'test_id_3', 'attributes': ['C23.888.144'], 'request': None},
